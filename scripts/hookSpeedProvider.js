ffed9af7c297cdf7560bec921ec391defd0f2f0d
Tnoin <Tnoin@users.noreply.github.com>
2021-08-06 21:50:05 +0200

    Added files
    because

---
+++ scripts/hookSpeedProvider.js
@@ -0,0 +1,38 @@
+console.log("TNOIN says it loaded");
+
+Hooks.on("init", function() {
+    console.log("TNOIN says init is in");
+});
+
+Hooks.on("ready", function() {
+    console.log("TNOIN says ready");
+});
+
+
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+    class ShadowrunSpeedProvider extends SpeedProvider {
+        get colors() {
+            return [
+                {id: "walk", default: 0x00FF00, name: "shadowrun5e.speeds.walk"},
+                {id: "dash", default: 0xFFFF00, name: "shadowrun5e.speeds.dash"},
+                {id: "run", default: 0xFF8000, name: "shadowrun5e.speeds.run"}
+            ]
+        }
+
+        getRanges(token) {
+            const baseSpeed = token.actor.data.data.movement.walk.value
+            const runSpeed = token.actor.data.data.movement.run.value
+            const sprintSpeed = token.actor.data.data.movement.sprint.value
+
+            // A character can always walk it's base speed and dash twice it's base speed
+            const ranges = [
+                {range: baseSpeed, color: "walk"},
+                {range: runSpeed, color: "dash"}
+            ]
+
+            return ranges
+        }
+    }
+
+    dragRuler.registerModule("shadowrun-drag-ruler-integration", ShadowrunSpeedProvider)
+})
